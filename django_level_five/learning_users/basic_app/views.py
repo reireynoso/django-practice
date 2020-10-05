8f41db7f6a49f7660e22a0cedfafea15e4014cb4
reireynoso <reireynoso@gmail.com>
2020-10-05 16:04:43 -0400

    login and authenticating existing user

--- django_level_five/learning_users/basic_app/views.py
+++ django_level_five/learning_users/basic_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from basic_app.forms import UserForm, UserProfileInfoForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
+# if you want a view to be viewed only when a user is logged in
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def index(request):
     return render(request, 'basic_app/index.html')
+
+@login_required
+def special(request):
+    return HttpResponse("You are logged in, Nice!")
+
+@login_required # requires the user to be logged in to access the route using the decorator
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 def register(request):
     registered = False
@@ -36,3 +53,26 @@ def register(request):
         profile_form = UserProfileInfoForm()
 
     return render(request, 'basic_app/registration.html', {'user_form': user_form, 'profile_form': profile_form, 'registered':registered})
+
+def user_login(request):
+    if request.method == "POST":
+        # get username from form submissiong
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # django built it authentication
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                # redirect back to home page
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Account not active")
+        else:
+            print("Someone tried to login and failed")
+            print("Username: {} and password {}".format(username,password))
+            return HttpResponse("Invalid login details supplied!")
+    else:
+        return render(request,'basic_app/login.html', {})
