3e50d048476b7c10b1b5b9fe0124bc9ffe5ef9b7
reireynoso <reireynoso@gmail.com>
2020-10-04 22:28:02 -0400

    user registration

--- django_level_five/learning_users/learning_users/urls.py
+++ django_level_five/learning_users/learning_users/urls.py
@@ -20,5 +20,5 @@ from basic_app import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('admin/', admin.site.urls),
-    path("bascic_app/", include('basic_app.urls'))
+    path("basic_app/", include('basic_app.urls'))
 ]
